37e0b0a8ce6a9dd9b60c01611c9dfb1713e48841
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2023-05-23 00:14:10 +0530

    Some changes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -16,6 +16,6 @@ router.put('/user/userupdate/:id',updateUserById)
 router.get('/user/finduser/:id',findUserById)
 router.delete('/user/deleteuser/:id',deleteUserById)
 router.get('/swipeRight', swipeRight)
-router.get('/gg', passport.authenticate('jwt', { session: false }),createConversation)
+router.get('/gg', isAuth,createConversation)
 
 export default router
