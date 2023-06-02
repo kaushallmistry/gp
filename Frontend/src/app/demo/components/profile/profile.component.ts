a0f76f46a3e558fe1994597b9e99b606c0b7dbaf
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2023-06-02 22:02:04 +0530

    fixed

--- Frontend/src/app/demo/components/profile/profile.component.ts
+++ Frontend/src/app/demo/components/profile/profile.component.ts
@@ -1,10 +1,22 @@
 import { AfterViewInit, Component,OnDestroy } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { GamesApiService } from '../../service/games-api.service';
-import { Subscription } from 'rxjs';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../service/Api.service';
+
 export interface Fruit {
   name: string;
+}
+
+export interface Profile{
+  id:string,
+  username:string,
+  email:string,
+  bio:string,
+  avatar:string,
+  games:string[]
 }
 @Component({
   selector: 'app-profile',
@@ -15,12 +27,17 @@ export class ProfileComponent implements AfterViewInit, OnDestroy {
 
   gamesData:{title:string; thumbnail:string; id:string}[]=[];
 
-  selectedItem: any;
-
   subs = new Subscription()
-  constructor(private gamesApiService:GamesApiService){
+  constructor(
+    private gamesApiService:GamesApiService,
+    public activatedRoute: ActivatedRoute,
+    private router: Router,
+    private ApiService:ApiService,
+    ){
 
   }
+  
+
   ngOnDestroy(): void {
     this.subs.unsubscribe()
   }
@@ -36,11 +53,30 @@ export class ProfileComponent implements AfterViewInit, OnDestroy {
     return window.innerWidth < 1280 && window.innerWidth  > 700;
   }
 
+  set document (doc:Profile){
+      
+    if(doc == null){
+      this.router.navigate(['/'])
+    }
+    else{
+
+      this.formGroup.setValue({
+        avatar:doc?.avatar,
+        bio:doc?.bio,
+        games:doc.games as any,
+        username:doc.username
+      })
+
+    }
+
+    this.formGroup.enable()
+  }
+
   formGroup = new FormGroup({
-    username: new FormControl(null, [Validators.required]),
-    bio: new FormControl(null,[Validators.required]),
-    games: new FormControl(null,[]),
-    avater: new FormControl(null,[])
+    username: new FormControl<string|null>(null, [Validators.required]),
+    bio: new FormControl<string| null>(null,[Validators.required]),
+    games: new FormArray ([]),
+    avatar: new FormControl<string| null>(null,[])
   })
 
   ngAfterViewInit(): void {
@@ -48,19 +84,21 @@ export class ProfileComponent implements AfterViewInit, OnDestroy {
     const subs1= this.gamesApiService.RetriveImages().subscribe(v => {this.gamesData = v.slice(0, 20);
       console.log(this.gamesData)
     })
+    const subs2 = this.activatedRoute.paramMap.pipe(
+      map(params => params.get('id')),
+      distinctUntilChanged(),
+      filter(id => !!id ),
+      tap(id=> this.formGroup.disable()),
+      switchMap(id =>this.ApiService.getCurrentUser(id as string))
+    ).subscribe(v =>{
+      console.log()
+      this.document = v as Profile})
 
     this.subs.add(subs1)
+    this.subs.add(subs2)
 
   }
 
-  getSelectedItem(item: any) {
-    this.selectedItem = item;
-    const selectedItem = item.thumbnail;
-    console.log("Selected item: ", item?.thumbnail);
-    this.formGroup.patchValue({
-        avater: selectedItem
-    });
-}
 
 
   fruits: Fruit[] = [];
@@ -87,3 +125,131 @@ export class ProfileComponent implements AfterViewInit, OnDestroy {
 
 
 }
+
+// import { AfterViewInit, Component,OnDestroy } from '@angular/core';
+// import { MatChipInputEvent } from '@angular/material/chips';
+// import { GamesApiService } from '../../service/games-api.service';
+// import { Subscription, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+// import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { ApiService } from '../../service/Api.service';
+
+// export interface Fruit {
+//   name: string;
+// }
+
+// export interface Profile{
+//   id:string,
+//   username:string,
+//   email:string,
+//   bio:string,
+//   avatar:string,
+//   games:string[]
+// }
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.scss']
+// })
+// export class ProfileComponent implements AfterViewInit, OnDestroy {
+
+//   gamesData:{title:string; thumbnail:string; id:string}[]=[];
+
+//   subs = new Subscription()
+//   constructor(
+//     private gamesApiService:GamesApiService,
+//     public activatedRoute: ActivatedRoute,
+//     private router: Router,
+//     private ApiService:ApiService,
+//     ){
+
+//   }
+  
+
+//   ngOnDestroy(): void {
+//     this.subs.unsubscribe()
+//   }
+//   get onMobile(): boolean {
+//     return window.innerWidth <= 700;
+//   }
+
+//   get onDesktop(): boolean {
+//     return window.innerWidth > 1280;
+//   }
+
+//  get  onTab(): boolean{
+//     return window.innerWidth < 1280 && window.innerWidth  > 700;
+//   }
+
+//   set document (doc:Profile){
+      
+//     if(doc == null){
+//       this.router.navigate(['/'])
+//     }
+//     else{
+
+//       this.formGroup.setValue({
+//         avatar:doc?.avatar,
+//         bio:doc?.bio,
+//         games:doc.games as any,
+//         username:doc.username
+//       })
+
+//     }
+
+//     this.formGroup.enable()
+//   }
+
+//   formGroup = new FormGroup({
+//     username: new FormControl<string|null>(null, [Validators.required]),
+//     bio: new FormControl<string| null>(null,[Validators.required]),
+//     games: new FormArray ([]),
+//     avatar: new FormControl<string| null>(null,[])
+//   })
+
+//   ngAfterViewInit(): void {
+
+//     const subs1= this.gamesApiService.RetriveImages().subscribe(v => {this.gamesData = v.slice(0, 20);
+//       console.log(this.gamesData)
+//     })
+//     const subs2 = this.activatedRoute.paramMap.pipe(
+//       map(params => params.get('id')),
+//       distinctUntilChanged(),
+//       filter(id => !!id ),
+//       tap(id=> this.formGroup.disable()),
+//       switchMap(id =>this.ApiService.getCurrentUser(id as string))
+//     ).subscribe(v =>{
+//       console.log()
+//       this.document = v as Profile})
+
+//     this.subs.add(subs1)
+//     this.subs.add(subs2)
+
+//   }
+
+
+
+//   fruits: Fruit[] = [];
+
+//   add(event: MatChipInputEvent): void {
+//     const value = (event.value || '').trim();
+
+//     // Add our fruit
+//     if (value) {
+//       this.fruits.push({name: value});
+//     }
+
+//     // Clear the input value
+//     event.chipInput!.clear();
+//   }
+
+//   remove(fruit: Fruit): void {
+//     const index = this.fruits.indexOf(fruit);
+
+//     if (index >= 0) {
+//       this.fruits.splice(index, 1);
+//     }
+//   }
+
+
+// }
